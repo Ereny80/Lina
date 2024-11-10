7783793fc7fb6e6711a6205fd37b2176a7dd608d
Ereny80 <134388440+Ereny80@users.noreply.github.com>
2024-11-10 21:04:38 +0300

    Add files via upload

---
+++ lina.js
@@ -0,0 +1,4 @@
+document.getElementById("showMessageButton").addEventListener("click", function() {
+    var specialMessage = document.getElementById("specialMessage");
+    specialMessage.classList.toggle("hidden");
+});
